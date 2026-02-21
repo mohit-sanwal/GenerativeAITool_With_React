c8b9dc83b19ea136b2259c6b17eaa7afc92ee4d4
mohitSanwal <mohit123ongraph@gmail.com>
2026-02-21 19:27:18 +0530

    GROQ_API_URL added to util folder

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import OpenAI from "openai";
-import {GROQ_API_URL} from './utils/urls';
+import {GROQ_API_URL} from './utils/urls.js';
 
 dotenv.config();
 
