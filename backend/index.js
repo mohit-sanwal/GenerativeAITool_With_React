08a898d25e8323d70b587b4ee6730d60fc1f14c9
mohitSanwal <mohit123ongraph@gmail.com>
2026-02-21 15:06:56 +0530

    dynamic port added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 const groq = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
@@ -31,6 +32,6 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(5000, () =>
+app.listen(PORT, () =>
   console.log("Server running on port 5000")
 );
